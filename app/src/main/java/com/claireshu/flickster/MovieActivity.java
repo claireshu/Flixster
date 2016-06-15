a4393f71816b377e3df26ae778768bf43882ae92
Claire Shu <claireshu@fb.com>
2016-06-15 16:20:47 -0700

    Updated with refreshing and placeholder images

--- app/src/main/java/com/claireshu/flickster/MovieActivity.java
+++ app/src/main/java/com/claireshu/flickster/MovieActivity.java
@@ -1,6 +1,7 @@
 package com.claireshu.flickster;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ListView;
@@ -21,11 +22,27 @@ public class MovieActivity extends AppCompatActivity {
     ArrayList<Movie> movies;
     ListView lvMovies;
     MoviesAdapter adapter;
+    private SwipeRefreshLayout swipeContainer;
+    AsyncHttpClient client;
+    String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                fetchTimelineAsync(0);
+            }
+        });
+
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
 
 
         lvMovies = (ListView) findViewById(R.id.lvMovies);
@@ -35,9 +52,9 @@ public class MovieActivity extends AppCompatActivity {
             lvMovies.setAdapter(adapter);
         }
 
-        String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+        url = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
 
-        AsyncHttpClient client = new AsyncHttpClient();
+        client = new AsyncHttpClient();
         client.get(url, new JsonHttpResponseHandler(){
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
@@ -59,4 +76,32 @@ public class MovieActivity extends AppCompatActivity {
             }
         });
     }
+
+    public void fetchTimelineAsync(int page) {
+        client.get(url, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                JSONArray movieJsonResults = null;
+                try {
+                    movieJsonResults = response.getJSONArray("results");
+                    movies.addAll(Movie.fromJsonArray(movieJsonResults));
+                    adapter.notifyDataSetChanged();
+                    Log.d("DEBUG", movieJsonResults.toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                // Remember to CLEAR OUT old items before appending in the new ones
+                adapter.clear();
+                // ...the data has come back, add new items to your adapter...
+                adapter.addAll(Movie.fromJsonArray(movieJsonResults));
+                // Now we call setRefreshing(false) to signal refresh has finished
+                swipeContainer.setRefreshing(false);
+            }
+
+            public void onFailure(Throwable e) {
+                Log.d("DEBUG", "Fetch timeline error: " + e.toString());
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/claireshu/flickster/MoviesAdapter.java
+++ app/src/main/java/com/claireshu/flickster/MoviesAdapter.java
@@ -20,7 +20,12 @@ import jp.wasabeef.picasso.transformations.RoundedCornersTransformation;
  * Created by claireshu on 6/15/16.
  */
 public class MoviesAdapter extends ArrayAdapter<Movie> {
-    
+    private static class ViewHolder {
+        TextView tvTitle;
+        TextView tvOverview;
+        ImageView ivPoster;
+    }
+
     public MoviesAdapter (Context context, ArrayList<Movie> movies) {
         super(context, R.layout.item_movie, movies);
     }
@@ -30,29 +35,38 @@ public class MoviesAdapter extends ArrayAdapter<Movie> {
         // Get the data item for this position
         Movie movie = getItem(position);
 
+        ViewHolder viewHolder;
+
         // Check if an existing view is being reused, otherwise inflate the view
         if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_movie, parent, false);
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.item_movie, parent, false);
+            viewHolder.tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+            viewHolder.tvOverview = (TextView) convertView.findViewById(R.id.tvOverview);
+            viewHolder.ivPoster = (ImageView) convertView.findViewById(R.id.ivPoster);
+
+            convertView.setTag(viewHolder);
+        }  else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
-        // Lookup view for data population
-        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
-        ImageView ivPoster = (ImageView) convertView.findViewById(R.id.ivPoster);
-        TextView tvOverview = (TextView) convertView.findViewById(R.id.tvOverview);
-
         // Populate the data into the template view using the data object
-        tvTitle.setText(movie.getTitle());
-        tvOverview.setText(movie.getOverview());
+        viewHolder.tvTitle.setText(movie.getTitle());
+        viewHolder.tvOverview.setText(movie.getOverview());
         String imageUri = null;
         Context context = getContext();
         if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
             imageUri = movie.getBackdropUrl();
+            Picasso.with(getContext()).load(imageUri)
+                    .placeholder(R.drawable.land_placeholder)
+                    .transform(new RoundedCornersTransformation(10, 10)).into(viewHolder.ivPoster);
         } else if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
             imageUri = movie.getPosterUrl();
+            Picasso.with(getContext()).load(imageUri)
+                    .placeholder(R.drawable.portrait_placeholder)
+                    .transform(new RoundedCornersTransformation(10, 10)).into(viewHolder.ivPoster);
         }
-
-        Picasso.with(getContext()).load(imageUri)
-                .transform(new RoundedCornersTransformation(10, 10)).into(ivPoster);
 
         //Log.d("MoviesAdapter", "Position:" + position);
 
